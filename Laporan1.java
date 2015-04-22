edf903438c1f18cbfd8e56f751e0265672b3eb51
nurfajrinur <60200112050@uin-alauddin.ac.id>
2015-04-22 12:41:06 +0800

    Create Laporan1.java

---
+++ Laporan1.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package TUGAS;
+
+/**
+ * @author Fajri
+ *
+ */
+public class LAP1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String praktikum1 = "biodata";
+		System.out.println(praktikum1);
+		biodata();
+		perkalian();
+		
+	}
+	public static void biodata()
+	{
+		System.out.println("nurfajri");
+		System.out.println("60200112050");
+		System.out.println("kls c");
+	}
+	public static void perkalian()
+	{
+		int a = 5*5;
+		double b = 5.5*5;
+		System.out.println("perkalian");
+		System.out.println("hasilnya adalah"+""+a);
+		System.out.println("hasilnya adalah"+""+b);
+		
+	}
+	
+}
